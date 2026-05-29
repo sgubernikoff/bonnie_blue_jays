56f65e32d7c5e4fc349d3c2822cc298789883d79
Sam Gubernikoff <sgubernikoff@gmail.com>
2026-05-29 17:38:58 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Schedule from './pages/Schedule'
-import Stats from './pages/Stats'
-import Gallery from './pages/Gallery'
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Schedule from "./pages/Schedule";
+import Stats from "./pages/Stats";
+import Gallery from "./pages/Gallery";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
-        <Route path="/"         element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/schedule" element={<Schedule />} />
-        <Route path="/stats"    element={<Stats />} />
-        <Route path="/gallery"  element={<Gallery />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/gallery" element={<Gallery />} />
       </Routes>
-    </BrowserRouter>
-  )
+    </HashRouter>
+  );
 }
